936c9b9741a5dd0e561584a14b2d3f047c010f3d
nauxqouh <ntxhuong04@gmail.com>
2026-07-04 12:13:57 +0700

    add depth params

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -50,6 +50,7 @@ class ModelParams(ParamGroup):
         self._source_path = ""
         self._model_path = ""
         self._images = "images"
+        self._depths = ""
         self._resolution = -1
         self._white_background = False
         self.data_device = "cuda"
@@ -92,6 +93,9 @@ class OptimizationParams(ParamGroup):
         self.densify_from_iter = 500
         self.densify_until_iter = 15_000
         self.densify_grad_threshold = 0.0002
+        self.depth_l1_weight_init = 1.0
+        self.depth_l1_weight_final = 0.01
+        self.random_background = False
         super().__init__(parser, "Optimization Parameters")
 
 def get_combined_args(parser : ArgumentParser):
